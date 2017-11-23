4e7f05a3017c13ef0759606708479e18655f3bc9
olga <o.sopilniak@gmail.com>
2017-11-23 05:45:30 +0300

    Remove unnecessary import, fix counter

--- static/index.js
+++ static/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import VuePaginate from 'vue-paginate'
 import VeeValidate from 'vee-validate'
-import VueScript2 from 'vue-script2'
 
 /* '@' is alias for static directory */
 import Root from '@/components/Root.vue'
@@ -10,7 +9,6 @@ import store from './store';
 
 Vue.use(VuePaginate);
 Vue.use(VeeValidate);
-Vue.use(VueScript2);
 
 new Vue({
     el: '#app',
