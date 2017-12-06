fba5213507f9151f97deaf37a5077f6fa8fb1837
Olga Sopilnyak <o.sopilniak@gmail.com>
2017-12-06 16:06:30 +0300

    Fix loading animation

--- static/components/GLoading.vue
+++ static/components/GLoading.vue
@@ -12,24 +12,27 @@
     export default {
         name: 'GLoading',
         mounted() {
+            let animationDuration = 700;
             $(document).ready(function() {
-            function runAnimation() {
                 let loadingLeft = $('.loading-1');
-                loadingLeft.animate({opacity:'1'}, 600);
-                loadingLeft.animate({opacity:'0.5'}, 600, runAnimation);
-            setTimeout(function() {
                 let loadingCenter = $('.loading-2');
-                loadingCenter.animate({opacity: '1'}, 600);
-                loadingCenter.animate({opacity: '0.5'}, 600, runAnimation);
-                }, 300);
-            setTimeout(function() {
                 let loadingRight = $('.loading-3');
-                loadingRight.animate({opacity: '1'}, 600);
-                loadingRight.animate({opacity: '0.5'}, 600, runAnimation);
-                }, 600);
-            }
-            runAnimation();
-        });
+                function runAnimation() {
+                    loadingLeft.animate({opacity:'1'}, animationDuration);
+                    loadingLeft.animate({opacity:'0.5'}, animationDuration, runAnimation);
+                    loadingCenter.animate({opacity: '1'}, animationDuration);
+                    loadingCenter.animate({opacity: '0.5'}, animationDuration, runAnimation);
+                    loadingRight.animate({opacity: '1'}, animationDuration);
+                    loadingRight.animate({opacity: '0.5'}, animationDuration, runAnimation);
+                }
+                loadingLeft.animate({opacity: '1'}, animationDuration);
+                loadingLeft.animate({opacity:'0.5'}, animationDuration, runAnimation);
+                loadingCenter.delay(animationDuration / 3).animate({opacity: '1'}, animationDuration);
+                loadingCenter.animate({opacity: '0.5'}, animationDuration, runAnimation);
+                loadingRight.delay(animationDuration * 2 / 3).animate({opacity: '1'}, animationDuration);
+                loadingRight.animate({opacity: '0.5'}, animationDuration, runAnimation);
+                runAnimation();
+            });
         }
     }
 </script>
@@ -41,6 +44,7 @@
         width: 20px;
         height: 20px;
         background: rgba(55,80,132,1.0);
+        opacity: 0.5;
     }
 
     .loading-2 {
@@ -48,7 +52,7 @@
         width: 20px;
         height: 20px;
         background: rgba(55,80,132,1.0);
-        opacity: 0.5;
+        opacity: 0.75;
     }
 
     .loading-3 {
@@ -56,7 +60,7 @@
         width: 20px;
         height: 20px;
         background: rgba(55,80,132,1.0);
-        opacity: 0.3;
+        opacity: 0.8;
     }
 
     .loading {
